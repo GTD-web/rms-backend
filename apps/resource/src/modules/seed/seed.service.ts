2bf09ce15519377ee14253eb3f5bb8307a7a4ecf
Kyuhyun Kim <ggh163700@gmail.com>
2025-05-15 10:36:27 +0900

    feat: 시드 서비스 추가 및 통계 컨트롤러에 시드 엔드포인트 구현

--- apps/resource/src/modules/seed/seed.service.ts
+++ apps/resource/src/modules/seed/seed.service.ts
@@ -27,7 +27,7 @@ export class SeedService implements OnModuleInit {
         // await this.seed();
     }
 
-    private async seed() {
+    async seed(employeeId: string) {
         // 기본 리소스 그룹 생성
         const defaultResourceGroup = await this.resourceGroupRepository.find();
 
@@ -66,7 +66,7 @@ export class SeedService implements OnModuleInit {
                     }
                     await this.resourceManagerRepository.save({
                         resourceId: savedResource.resourceId,
-                        employeeId: 'bedf827a-a374-4549-9c84-90698fbd9d51',
+                        employeeId: employeeId,
                     });
                 }
             }
