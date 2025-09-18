1ea972eecc2e9d0d387ad8da7c2bcfa14a43ced4
Kyuhyun Kim <ggh163700@gmail.com>
2025-09-18 17:59:29 +0900

    feat: 취소된 일정에 deletedAt 및 완료 사유 설정을 위한 새로운 마이그레이션 파일 추가 - 취소된 일정의 deletedAt을 시작 날짜 1일 전으로 설정하고, 완료 사유를 '일정 취소'로 업데이트하는 마이그레이션을 추가하여 데이터 관리 개선

--- libs/migrations/1756193000000-AddForeignKeysToScheduleRelations.ts
+++ libs/migrations/1756193000000-AddForeignKeysToScheduleRelations.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddForeignKeysToScheduleRelations1756192900000 implements MigrationInterface {
-    name = 'AddForeignKeysToScheduleRelations1756192900000';
+export class AddForeignKeysToScheduleRelations1756193000000 implements MigrationInterface {
+    name = 'AddForeignKeysToScheduleRelations1756193000000';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // ScheduleRelation과 Schedule 간의 외래키 제약조건 추가
